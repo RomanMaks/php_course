22a187921bd12c4b5e0e3c7187ba7da7141cabcd
Roman Maksimov <maksimov.ro.al@gmail.com>
2019-06-13 09:02:31 +0300

    Исправил миграцию

--- src/Migrations/Version20190612150001.php
+++ src/Migrations/Version20190612150001.php
@@ -25,9 +25,9 @@ final class Version20190612150001 extends AbstractMigration
             'Migration can only be executed safely on \'postgresql\'.'
         );
 
-        $this->addSql('CREATE SEQUENCE user_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE users_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('
-CREATE TABLE "user" (
+CREATE TABLE users (
   id INT NOT NULL,
   password TEXT,
   first_name VARCHAR(100) NOT NULL,
@@ -35,12 +35,40 @@ CREATE TABLE "user" (
   middle_name VARCHAR (100),
   phone VARCHAR(11) NOT NULL,
   email VARCHAR(100) NOT NULL,
-  created_at TIMESTAMP NOT NULL current_timestamp(),
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP,
   deleted_at TIMESTAMP,
   PRIMARY KEY(id)
   )
         ');
+        $this->addSql('CREATE UNIQUE INDEX uniq_users_email ON users (email)');
+        $this->addSql('CREATE UNIQUE INDEX uniq_users_phone ON users (phone)');
+
+        $this->addSql('
+CREATE TABLE user_roles (
+  user_id INT NOT NULL,
+  role_id INT NOT NULL,
+  PRIMARY KEY(user_id, role_id)
+)'
+        );
+        $this->addSql('CREATE INDEX idx_user_roles_user_id ON user_roles (user_id)');
+        $this->addSql('CREATE INDEX idx_user_roles_role_id ON user_roles (role_id)');
+
+        $this->addSql('CREATE SEQUENCE roles_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('
+CREATE TABLE roles (
+  id SERIAL,
+  codename VARCHAR(20) NOT NULL,
+  title VARCHAR(50) NOT NULL,
+  PRIMARY KEY(id)
+)'
+        );
+        $this->addSql('CREATE UNIQUE INDEX uniq_roles_codename ON roles (codename)');
+        $this->addSql('ALTER TABLE user_roles ADD CONSTRAINT fk_user_roles_user_id FOREIGN KEY (user_id) REFERENCES users (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE user_roles ADD CONSTRAINT fk_user_roles_role_id FOREIGN KEY (role_id) REFERENCES roles (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+
+        $this->addSql('INSERT INTO roles (codename, title) VALUES (\'ROLE_USER\', \'Пользователь\')');
+        $this->addSql('INSERT INTO roles (codename, title) VALUES (\'ROLE_ADMIN\', \'Администратор\')');
     }
 
     public function down(Schema $schema) : void
@@ -51,7 +79,12 @@ CREATE TABLE "user" (
             'Migration can only be executed safely on \'postgresql\'.'
         );
 
-        $this->addSql('DROP SEQUENCE user_id_seq CASCADE');
-        $this->addSql('DROP TABLE "user"');
+        $this->addSql('ALTER TABLE user_roles DROP CONSTRAINT fk_user_roles_user_id');
+        $this->addSql('ALTER TABLE user_roles DROP CONSTRAINT fk_user_roles_role_id');
+        $this->addSql('DROP SEQUENCE users_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE roles_id_seq CASCADE');
+        $this->addSql('DROP TABLE users');
+        $this->addSql('DROP TABLE user_roles');
+        $this->addSql('DROP TABLE roles');
     }
 }
